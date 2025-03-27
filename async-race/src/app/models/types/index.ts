9818832d812c40c07efe4af8f281220995fa778b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-27 19:41:06 +0300

    feat(types): add index barrel file

---
+++ async-race/src/app/models/types/index.ts
@@ -0,0 +1,8 @@
+export type { EndpointAttributes } from './api-types/endpoint-attributes';
+export type { EndpointRequest } from './api-types/endpoint-request';
+export type { EndpointResponse } from './api-types/endpoint-response';
+export type { MatchEndpointWithRequest } from './api-types/match-endpoint-with-request';
+export type { PickAttributes } from './api-types/pick-attributes';
+export type { Car } from './car';
+export type { Endpoint } from './endpoints/endpoint';
+export type { Winner } from './winner';
