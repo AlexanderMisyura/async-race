c53dffe52ee8553ad02fadb7f30b26d51b5b96c8
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-03-27 19:34:32 +0300

    feat(types): add api enums and interfaces

---
+++ async-race/src/app/models/interfaces/index.ts
@@ -0,0 +1,6 @@
+export type { CarRequest } from './car/car-request';
+export type { CarResponse } from './car/car-response';
+export type { EngineDriveModeResponse } from './engine/engine-drive-mode-response';
+export type { EngineResponse } from './engine/engine-response';
+export type { WinnerRequest } from './winner/winner-request';
+export type { WinnerResponse } from './winner/winner-response';
