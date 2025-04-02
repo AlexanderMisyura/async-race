69f0889408cafa0c81484e5b7e41a07a23661932
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-04-02 08:16:41 +0300

    fix(configs): update default winners limit per page

--- async-race/src/app/config.ts
+++ async-race/src/app/config.ts
@@ -2,7 +2,7 @@ import { WinnersOrderParameter, WinnersSortParameter } from '@ts-enums';
 
 export default {
   API_URL: 'http://localhost:3000/',
-  DEFAULT_WINNERS_PER_PAGE: 7,
+  DEFAULT_WINNERS_PER_PAGE: 10,
   DEFAULT_WINNERS_PAGE_NUMBER: 1,
   DEFAULT_WINNERS_SORT_BY: WinnersSortParameter.TIME,
   DEFAULT_WINNERS_SORT_ORDER: WinnersOrderParameter.ASC,
