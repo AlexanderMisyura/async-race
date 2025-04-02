41af8f37e368a32710300877c262c20304509eca
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-04-03 02:47:23 +0300

    feat(components): add winner-record component

---
+++ async-race/src/app/components/winner-record/winner-record.ts
@@ -0,0 +1,81 @@
+import BaseComponent from '@components/base-component';
+import tag from '@components/utility-components';
+import machine from '@state-machine/machine';
+import type { Car, MachinePayload, Winner } from '@ts-types';
+
+import * as styles from './winner-record.module.scss';
+
+export default class WinnerRecord extends BaseComponent<'div'> {
+  private id: BaseComponent<'div'> | undefined;
+  private image: BaseComponent<'div'> | undefined;
+  private name: BaseComponent<'div'> | undefined;
+  private wins: BaseComponent<'div'> | undefined;
+  private bestTime: BaseComponent<'div'> | undefined;
+
+  constructor(
+    private winnerData: Winner,
+    seqNumber: number
+  ) {
+    super({ elementTag: 'div', classes: [styles.winnerRecord] });
+
+    void machine.makeTransition(machine.value, 'getCar', {
+      carId: this.winnerData.id,
+    });
+
+    const recordFields = this.createRecordFields(seqNumber);
+
+    this.appendChildren(...recordFields);
+
+    machine.on(
+      machine.events.machineStateChanged,
+      this.handleStateChange.bind(this)
+    );
+  }
+
+  private handleStateChange(payload: MachinePayload): void {
+    const { trigger, getFullContext, contextData } = payload;
+
+    switch (trigger) {
+      case 'updateCar': {
+        if (contextData?.car?.id !== this.winnerData.id) break;
+
+        this.updateRecord(contextData.car);
+
+        break;
+      }
+
+      case 'getCar': {
+        if (contextData?.carId !== this.winnerData.id) break;
+        const { car } = getFullContext();
+        if (car) this.updateRecord(car);
+      }
+    }
+  }
+
+  private createRecordFields(seqNumber: number): BaseComponent<'div'>[] {
+    const number = tag.div({
+      classes: [styles.number],
+      text: seqNumber.toString(),
+    });
+    this.image = tag.div({
+      classes: [styles.tractorImage],
+      text: 'tractor',
+    });
+    this.name = tag.div({ classes: [styles.name] });
+    this.wins = tag.div({
+      classes: [styles.wins],
+      text: this.winnerData.wins.toString(),
+    });
+    this.bestTime = tag.div({
+      classes: [styles.bestTime],
+      text: this.winnerData.time.toString(),
+    });
+
+    return [number, this.image, this.name, this.wins, this.bestTime];
+  }
+
+  private updateRecord(car: Car): void {
+    if (this.image) this.image.getElement().style.color = car.color;
+    this.name?.setText(car.name);
+  }
+}
